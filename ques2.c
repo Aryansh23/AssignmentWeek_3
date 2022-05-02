26f5af78e6886dbb39c525b9de5882dabb839a25
Aryansh23 <98678055+Aryansh23@users.noreply.github.com>
2022-05-02 21:33:43 +0530

    Update and rename ques2 (1).c to ques2.c

--- ques2.c
+++ ques2.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    if (checkPrime(num, num / 2) == 1)
+    if (checkPrime(num, num/2)==1)
         printf("Given number is prime number");
     else
         printf("Given number is not prime number");
@@ -15,15 +15,18 @@ int main()
 }
 int checkPrime(int num, int i)
 {
-    if (i != 1) {
-        if (num % i != 0) {
+    if (i != 1)
+    {
+        if (num % i != 0)
+        {
             return checkPrime(num, i - 1);
         }
         else {
             return 0;
         }
     }
-    else {
+    else
+    {
         return 1;
     }
 }
