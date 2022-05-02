2e1d243b0235975c96c895dc237404f0ed7114f5
Aryansh23 <98678055+Aryansh23@users.noreply.github.com>
2022-05-02 21:12:36 +0530

    Add files via upload

---
+++ q1 (1).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int gcd(int,int); 
+int main() 
+{   int a,b,g; 
+    printf("Enter two numbers"); 
+    scanf("%d%d",&a,&b); 
+    g=gcd(a,b); 
+    printf("GCD of %d and %d is %d",a,b,g); 
+} 
+int gcd(int a, int b) 
+ { 
+    if(b==0)
+     return a; 
+    else 
+     return gcd(b,a%b); 
+}
